598d93757edecb406205446fd9d68350263ae238
Proponent-8247 <137363254+Proponent-8247@users.noreply.github.com>
2026-09-08 14:05:56 -0600

    test: reproduce extended transfer cancellation and queue races

--- tests/Run-AuditRegression.ps1
+++ tests/Run-AuditRegression.ps1
@@ -14,7 +14,7 @@ if (!(Test-Path $csc)) { throw '.NET Framework C# compiler was not found.' }
 if (!(Test-Path $ApplicationPath)) { throw "Application assembly was not found: $ApplicationPath" }
 $exe = Join-Path $EvidenceDirectory 'AuditRegression.exe'
 $sources = @(Get-ChildItem (Join-Path $PSScriptRoot 'AuditRegression*.cs') | ForEach-Object { $_.FullName })
-& $csc /nologo /langversion:5 /target:exe "/out:$exe" /reference:System.Core.dll /reference:System.Xml.Linq.dll @sources
+& $csc /nologo /langversion:5 /target:exe "/out:$exe" /reference:System.Core.dll /reference:System.Xml.Linq.dll /reference:System.Windows.Forms.dll /reference:System.Drawing.dll @sources
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 & $exe $ApplicationPath (Join-Path $EvidenceDirectory 'regression-results.xml') 2>&1 | Tee-Object -FilePath (Join-Path $EvidenceDirectory 'regression-console.txt')
 $code = $LASTEXITCODE

365146423d0deb572aa45a51ad1b3b2d28744461
Proponent-8247 <137363254+Proponent-8247@users.noreply.github.com>
2026-09-10 14:27:25 -0600

    test: activate repair wave 2 characterizations

--- tests/AuditRegression.cs
+++ tests/AuditRegression.cs
@@ -20,6 +20,7 @@ internal static partial class AuditRegression {
     static partial void RunUpdaterTests();
     static partial void RunPackagingTests();
     static partial void RunReconciliationTests();
+    static partial void RunRepairWave2Tests();
     private static Assembly App;
     private static readonly BindingFlags All = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
     private static readonly List<XElement> Results = new List<XElement>();
@@ -183,6 +184,7 @@ internal static partial class AuditRegression {
             RunUiTests();
             RunConverterTests();
             RunReconciliationTests();
+            RunRepairWave2Tests();
             RunUpdaterTests();
             RunPackagingTests();
         }

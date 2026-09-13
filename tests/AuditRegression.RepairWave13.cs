aaaef89514afa7176a1c2c89663b63bfe4ce8e52
Proponent-8247 <137363254+Proponent-8247@users.noreply.github.com>
2026-09-12 19:47:41 -0600

    test: characterize settings-owned tool update lifetime

---
+++ tests/AuditRegression.RepairWave13.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+internal static partial class AuditRegression {
+    private static void SettingsToolOperationOutlivesSettings() {
+        Type settings = T("youtube_dl_gui.frmSettings");
+        Type program = T("youtube_dl_gui.Program");
+        using (ManualResetEventSlim entered = new ManualResetEventSlim(false))
+        using (ManualResetEventSlim release = new ManualResetEventSlim(false)) {
+            Func<Task> work = () => Task.Run(() => {
+                entered.Set();
+                release.Wait(5000);
+            });
+
+            Task operation;
+            using (Form form = (Form)Activator.CreateInstance(settings)) {
+                operation = (Task)Call(settings, null, "RunApplicationOwnedToolOperationAsync", work);
+                PumpUntil(() => entered.IsSet, 5000, "Application-owned tool operation did not start");
+                Equal(true, ProgramHasRunningActions(program));
+                form.Dispose();
+                Equal(true, ProgramHasRunningActions(program));
+            }
+
+            release.Set();
+            operation.GetAwaiter().GetResult();
+            Equal(false, ProgramHasRunningActions(program));
+        }
+    }
+
+    private static void RunRepairWave13Tests() {
+        Test("POLICY_O022.SettingsToolOperationOutlivesSettings", SettingsToolOperationOutlivesSettings);
+    }
+}
